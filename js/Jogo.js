6719e5cb429db11cf8a9de6caa2d8335b53567ac
Wilson Ramos <hupcontato@gmail.com>
2020-04-10 17:35:02 -0300

    Melhorias no codigo

--- js/Jogo.js
+++ js/Jogo.js
@@ -2,33 +2,42 @@ class Jogo {
 
    constructor() {
       this.form = document.querySelector("form");
-      this.palavra = document.querySelector('#palavra')
-
+      this.palavra = document.querySelector('#palavra');
       this.letra = document.querySelector('#chute');
+
       this.palavras = ["PAULINHA", "WILSON"];
+      this.palavraSorteada = [];
       this.palavraMontada = [];
    }
 
    sorteiaPalavra() {
-
-      return this.palavras[Math.floor(Math.random() * this.palavras.length)].split('');
-   }
-
-
-   verificaLetra(event) {
-      event.preventDefault()
-
-      this.sorteiaPalavra().map((item, index) => {
-         let indexDaLetra = item.indexOf(this.letra.value.toUpperCase());
-         indexDaLetra >= 0 ? this.montaPalavra(index) : console.log('sorteia ' + this.sorteiaPalavra().length);
-      })
-   }
-
-   montaPalavra(indexDaLetra) {
-
-      this.palavraMontada[indexDaLetra] = this.letra.value.toUpperCase();
-      console.log(this.palavraMontada)
+      this.palavraSorteada = this.palavras[Math.floor(Math.random() * this.palavras.length)].split('');
       
    }
 
+   verificaLetra(event) {
+      event.preventDefault();
+
+      this.palavraSorteada.map((item, index) => {
+         
+         let indexDaLetra = item.indexOf(this.letra.value.toUpperCase());
+         indexDaLetra >= 0 ? this.montaPalavra(index) : console.log();
+      })
+
+      this.limpa();
+   }
+
+   montaPalavra(index) {
+
+      if (this.letra.value != '') {
+
+         this.palavraMontada[index] = this.letra.value.toUpperCase();
+         console.log(this.palavraMontada);
+      }
+   }
+
+   limpa() {
+      this.letra.value = '';
+      this.letra.focus()
+   }
 }
